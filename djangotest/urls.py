ec34eda90c12577e3d4587a0265efd8f6f215c84
ilia-boyko <90640565+ilia-boyko@users.noreply.github.com>
2025-09-05 21:19:17 +0300

    Update urls.py

--- djangotest/urls.py
+++ djangotest/urls.py
@@ -2,7 +2,7 @@
 URL configuration for djangotest project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,20 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from testapp import views
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("", views.index),
-    path("quote/<int:id>", views.quote),
     path("edit/<int:id>", views.edit),
     path("delete/<int:id>", views.delete),
     path("form", views.form, name="form"),
     path("postquote", views.postquote),
-    path("quotesjson", views.quotesjson),
-    path("loadquotes", views.loadquotes),
     path("rate", views.rate),
     path("dashboard", views.dashboard),
 ]
